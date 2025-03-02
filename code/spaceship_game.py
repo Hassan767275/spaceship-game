8d8ef1e3b4e7234d2d278506f5d7b6231e3a243f
Hassan7675 <hassanakhtar291@gmail.com>
2025-03-02 17:36:42 -0500

    displayed surfaces and now im leanring how to import picutres as images

--- code/spaceship_game.py
+++ code/spaceship_game.py
@@ -3,8 +3,17 @@ import pygame
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
 pygame.display.set_caption("Spaceship Game")
 running = True
+
+#create a surface to put on display surface
+surface = pygame.Surface((100, 200))
+surface.fill("pink")
+x = 100
+
+#importing an image
+player_surface = pygame.image.load("images/player.png")
 
 while running:
     #event loop
@@ -14,6 +23,8 @@ while running:
             running = False
     #draw the game
     # fill window with red color
-    display_surface.fill("blue")
+    display_surface.fill("lightslateblue")
+    x += 0.1
+    display_surface.blit(surface, (x, 150))
     pygame.display.update()
 pygame.quit()
